a0b4fc0959ce88b5fbe18d09e14b615228b2713c
irfanhathoti <irfanhathoti@gmail.com>
2023-06-19 13:07:27 +0530

    changes

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import fetch from 'cross-fetch';
 
-// const API_URL = 'http://localhost:3000/api/graphql';
-const API_URL = 'https://ssrreactflowf9455-n7cidehgba-uc.a.run.app/api/graphql';
+const API_URL = 'http://localhost:3000/api/graphql';
+// const API_URL = 'https://ssrreactflowf9455-n7cidehgba-uc.a.run.app/api/graphql';
 // https://us-central1-fluted-polymer-388415.cloudfunctions.net/graphql
 const link = createHttpLink({
   uri: API_URL,
