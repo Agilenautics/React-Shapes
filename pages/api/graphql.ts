89e0b4c1558628393ee18f913d028cacc6827024
irfanhathoti <irfanhathoti@gmail.com>
2023-09-20 16:54:30 +0530

    graphql changes

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -25,6 +25,15 @@
 // //   neo4j.auth.basic(process.env.USER_NAME, process.env.DB_PASSWORD)
 // // );
 
+
+// const neoSchema = new Neo4jGraphQL({ typeDefs, driver, resolvers });
+// const apolloServer = new ApolloServer({
+//   schema: await neoSchema.getSchema(),
+//   introspection: true,
+//   plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+// });
+
+// const startServer = apolloServer.start();
 // // @ts-ignore
 // export default async function handler(req, res) {
 //   res.setHeader("Access-Control-Allow-Credentials", "true");
@@ -36,18 +45,14 @@
 //     "Access-Control-Allow-Headers",
 //     "Origin, X-Requested-With, Content-Type, Accept"
 //   );
+//   await startServer
 //   if (req.method === "OPTIONS") {
 //     res.end();
 //     return false;
 //   }
 
-//   const neoSchema = new Neo4jGraphQL({ typeDefs, driver, resolvers });
-//   const apolloServer = new ApolloServer({
-//     schema: await neoSchema.getSchema(),
-//     introspection: true,
-//     plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
-//   });
-//   await apolloServer.start();
+
+
 //   await apolloServer.createHandler({
 //     // ? This is path on which the Apollo server is located
 //     path: "/api/graphql",
@@ -63,93 +68,35 @@
 
 
 
-
-
-// npm install @apollo/server express graphql cors body-parser
-// import { ApolloServer } from '@apollo/server';
-// import { expressMiddleware } from '@apollo/server/express4';
-// import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-// import express from 'express';
-// import http from 'http';
-// import cors from 'cors';
-// import pkg from 'body-parser';
-// const { json } = pkg;
-// import resolvers from './resolvers';
-// import typeDefs from './typeDefs'
-// import { Neo4jGraphQL } from '@neo4j/graphql';
-// import driver from './dbConnection';
-
-// interface MyContext {
-//   token?: String;
-// }
-
-// const app = express();
-// const httpServer = http.createServer(app);
-// const neoSchema = new Neo4jGraphQL({typeDefs,driver,resolvers})
-
-
-// export default async function createApolloServer(){
-//   const server = new ApolloServer<MyContext>({
-//     schema:await neoSchema.getSchema(),
-//     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-//   });
-//   await server.start();
-//   app.use(
-//     '/api/graphql',
-//     cors<cors.CorsRequest>(),
-//     json(),
-//     expressMiddleware(server, {
-//       context: async ({ req }) => ({ token: req.headers.token }),
-//     }),
-//   );
-
-//   await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-//   console.log(`🚀 Server ready at http://localhost:3000/graphql`);
-
-
-
-// }
-
-
-import { ApolloServer } from "@apollo/server";
-import driver from "./dbConnection";
-import resolvers from "./resolvers";
-import typeDefs from "./typeDefs";
-import express from 'express'
-import dotenv from 'dotenv'
-import http from "http"
-import cors from 'cors';
-import pkg from 'body-parser';
-const { json } = pkg;
+import { ApolloServer } from "apollo-server-micro";
 import { Neo4jGraphQL } from "@neo4j/graphql";
-import { expressMiddleware } from '@apollo/server/express4'
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import dotenv from "dotenv";
+import EventEmitter from "events";
+import resolvers from "./resolvers";
+import driver from "./dbConnection";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import typeDefs from "./typeDefs";
 
-type ResponseData = {
-  message: string
-}
+// ? The function below takes the path from the root directory
+// ? The file referrenced here contains the schema for GraphQL
+//const typeDefs = loadFile("pages/api/sdl.graphql");
 
-dotenv.config()
+EventEmitter.defaultMaxListeners = 15;
 
-const app = express();
-const httpServer = http.createServer(app)
+dotenv.config();
 
-const neoSchema = new Neo4jGraphQL({ driver, resolvers, typeDefs });
-const server = new ApolloServer({
-  schema: await neoSchema.getSchema(),
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-});
+// ? Here we provide authentication details for the Neo4j server
+// * This server is currently for development only, we will need to change
+// * to another server before production
+// const driver = neo4j.driver(
+//   // @ts-ignore
+//   process.env.DB_URL,
+//   // @ts-ignore
+//   neo4j.auth.basic(process.env.USER_NAME, process.env.DB_PASSWORD)
+// );
 
-const startServer = server.start()
-
-
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-
+// @ts-ignore
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Origin",
@@ -159,80 +106,113 @@ export default async function handler(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  await startServer;
   if (req.method === "OPTIONS") {
-    console.log("hi iam in request method options")
-    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, POST"); //this field i have newly
     res.end();
     return false;
   }
-  app.use(cors());
 
-
-
-  // app.use('/',(req,res)=>{
-  //   return res.send('hello')
-  // })
-
-
-
-
-
-
-  app.use(
-    '/api/graphql',
-    cors<cors.CorsRequest>(),
-    json(),
-    expressMiddleware(server, {
-      context: async ({ req, res }) => ({ req, res }),
-    }),
-  )(req, res);
+  const neoSchema = new Neo4jGraphQL({ typeDefs, driver, resolvers });
+  const apolloServer = new ApolloServer({
+    schema: await neoSchema.getSchema(),
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  });
+  await apolloServer.start();
+  await apolloServer.createHandler({
+    // ? This is path on which the Apollo server is located
+    path: "/api/graphql",
+  })(req, res);
 }
+
 export const config = {
   api: {
     bodyParser: false,
-    externalResolver: true,
   },
 };
 
-// import { ApolloServer } from "apollo-server-micro";
-// import typeDefs from "./typeDefs";
-// import resolvers from "./resolvers";
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import { Neo4jGraphQL } from "@neo4j/graphql";
-// import Cors from 'micro-cors'
-// // import cors from 'cors'
-// import dotenv from 'dotenv'
+
+
+
+
+
+
+
+
+
+// import { ApolloServer } from "@apollo/server";
 // import driver from "./dbConnection";
+// import resolvers from "./resolvers";
+// import typeDefs from "./typeDefs";
+// import express from 'express'
+// import dotenv from 'dotenv'
+// import http from "http"
+// import cors from 'cors';
+// import pkg from 'body-parser';
+// const { json } = pkg;
+// import { Neo4jGraphQL } from "@neo4j/graphql";
+// import { expressMiddleware } from '@apollo/server/express4'
+// import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+// import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+
 // dotenv.config()
 
-// const cors = Cors()
-// const neoSchema = new Neo4jGraphQL({ typeDefs, driver, resolvers })
-// const apolloServer = new ApolloServer({
-//   schema: await neoSchema.getSchema()
+// const app = express();
+// const httpServer = http.createServer(app)
+
+// const neoSchema = new Neo4jGraphQL({ driver, resolvers, typeDefs });
+// const server = new ApolloServer({
+//   schema: await neoSchema.getSchema(),
+//   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 // });
 
-// const startServer = apolloServer.start()
+// const startServer = server.start()
 
 
-// export default cors(async function handler(req: NextApiRequest, res: NextApiResponse) {
-//     if (req.method === "OPTIONS") {
-//       console.log("hi")
-//       res.end();
-//       return false;
-//     }
-//     await startServer;
-//     await apolloServer.createHandler({
-//       path: '/api/graphql'
-//     })(req, res);
+
+// export default async function handler(
+//   req:any,
+//   res: any
+// ) {
+
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "*"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   await startServer;
+//   if (req.method === "OPTIONS") {
+//     // res.setHeader("Access-Control-Allow-Methods", "OPTIONS, POST"); //this field i have newly
+//     res.end();
+//     return false;
 //   }
-// );
+//   app.use(cors());
+
+
+
+
+
+
+
+
+//   app.use(
+//     '/api/graphql',
+//     cors<cors.CorsRequest>(),
+//     json(),
+//     expressMiddleware(server, {
+//       context: async ({ req, res }) => ({ req, res }),
+//     }),
+//   )(req, res);
+// }
 // export const config = {
 //   api: {
 //     bodyParser: false,
-//     // externalResolver: true,
+//     externalResolver: true,
 //   },
 // };
-
-
 
