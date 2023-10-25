f54818ec9ed37629ff0161678bcb06a0842a1201
Vishnu <vishnu@Vishnus-MacBook-Air.local>
2023-10-25 10:52:37 +0530

    Added routes to Jest test file

--- components/Authentication/Login/index.test.js
+++ components/Authentication/Login/index.test.js
@@ -1,11 +1,25 @@
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
-import Login from './Login'
+import { useRouter } from 'next/router'; 
+import Login from './Login';
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn()
+}));
+
+const mockRouter = {
+  route: '/',
+  pathname: '/',
+  query: { projectId: 'some_value' },
+  asPath: '/',
+};
+
+useRouter.mockReturnValue(mockRouter); 
 
 describe("LoginTest", () => {
     it("Renders login page", () => {
       render(<Login />);
-       const result = screen.getAllByText("WELCOME");
-       console.log(result)
+      const result = screen.getAllByText("WELCOME");
+      console.log(result);
     });
-  });
+});
