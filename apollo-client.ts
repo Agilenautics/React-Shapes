c41681a89558933f498289de4c6d0a5805a5e127
Vishnu <vishnu@Vishnus-MacBook-Air.local>
2023-11-01 18:52:09 +0530

    Authentication using firebase and directives

--- apollo-client.ts
+++ apollo-client.ts
@@ -10,6 +10,14 @@ function getCookie(name: string): string | null {
 }
 
 
+function getCookie(name: string): string | null {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
+  return null;
+}
+
+
 const link = createHttpLink({
   uri: API_URL,
   fetch,
