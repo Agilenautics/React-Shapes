4117e6bd8ff77c80b44c916ebf07428e559ce188
Vishnu <vishnu@Vishnus-MacBook-Air.local>
2023-11-01 18:52:09 +0530

    Authentication using firebase and directives

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,18 +1,36 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 import fetch from "cross-fetch";
 // const API_URL = "http://localhost:3000/api/graphql"
 const API_URL = "https://react-flow-agile-livid.vercel.app/api/graphql"
+
+function getCookie(name: string): string | null {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
+  return null;
+}
+
 
 const link = createHttpLink({
   uri: API_URL,
   fetch,
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = getCookie('accessToken');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   uri: API_URL,
-  link,
+  link : authLink.concat(link),
 });
 
 export default client;
