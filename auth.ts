349a70ccd5981aa710ff004912f8eecfac024ccd
irfanhathoti <irfanhathoti@gmail.com>
2023-11-08 20:05:55 +0530

    authenticated new project

--- auth.ts
+++ auth.ts
@@ -18,6 +18,15 @@ import {
 //console.log(process.env.apiKey)
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCRTufqYpj4_3HdEYqzU4EQ3zTu-oQmTyw",
+//   authDomain: "react-flow-f9455.firebaseapp.com",
+//   projectId: "react-flow-f9455",
+//   storageBucket: "react-flow-f9455.appspot.com",
+//   messagingSenderId: "715694870988",
+//   appId: "1:715694870988:web:a79f88f5ecfd27b9004d69",
+//   measurementId: "G-JG4RFPR5TB"
+// };
 const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
@@ -27,12 +36,18 @@ const firebaseConfig = {
   appId: process.env.appId,
   measurementId: process.env.measurementId,
 };
+let app = initializeApp(firebaseConfig);
+// if (!firebase.getApps().length) {
+//   app = firebase.initializeApp(firebaseConfig);
+// } else {
+//   app = firebase.getApp();
+// }
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// export const  googleProvider = new GoogleAuthProvider();
-// export const facebookProvider = new FacebookAuthProvider();
-
+export const googleProvider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
+// const analytic = getAnalytics(app);
+// console.log(analytic)
 export const auth = getAuth(app);
 let remoteConfig: any;
 if (typeof window !== "undefined") {
