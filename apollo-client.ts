22742ded0d8e42959be2f1c0291af4e772ffa462
Agilenautics <anitha@agilenautics.com>
2023-12-14 15:08:03 +0200

    structure change branch rebased with main

--- apollo-client.ts
+++ apollo-client.ts
@@ -10,12 +10,6 @@ function getCookie(name: string): string | null {
 }
 
 
-function getCookie(name: string): string | null {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
-  return null;
-}
 
 
 const link = createHttpLink({
