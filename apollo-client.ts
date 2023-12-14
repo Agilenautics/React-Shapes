54d2a152e6d7137cbec6d4e7abcdb33c89dfee18
Agilenautics <anitha@agilenautics.com>
2023-12-14 15:08:03 +0200

    structure change branch rebased with main

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 import fetch from "cross-fetch";
-// const API_URL = "http://localhost:3000/api/graphql"
-const API_URL = "https://react-flow-agile-livid.vercel.app/api/graphql"
+const API_URL = "http://localhost:3000/api/graphql"
+//const API_URL = "https://react-flow-agile-livid.vercel.app/api/graphql"
 
 function getCookie(name: string): string | null {
   const value = `; ${document.cookie}`;
@@ -12,12 +12,6 @@ function getCookie(name: string): string | null {
 }
 
 
-function getCookie(name: string): string | null {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
-  return null;
-}
 
 
 const link = createHttpLink({
