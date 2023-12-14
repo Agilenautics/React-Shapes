f1961005ab01ad7747cec2904b1391fcf9b602ed
Agilenautics <anitha@agilenautics.com>
2023-12-14 15:24:25 +0200

    apollo client cookie issue and lock file updated

--- apollo-client.ts
+++ apollo-client.ts
@@ -4,12 +4,6 @@ import fetch from "cross-fetch";
 const API_URL = "http://localhost:3000/api/graphql"
 //const API_URL = "https://react-flow-agile-livid.vercel.app/api/graphql"
 
-function getCookie(name: string): string | null {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
-  return null;
-}
 
 
 
