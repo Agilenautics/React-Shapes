94ac28b4d76be5f194e6995d45e7cd7a357f434e
Agilenautics <anitha@agilenautics.com>
2023-12-14 15:24:25 +0200

    apollo client cookie issue and lock file updated

--- apollo-client.ts
+++ apollo-client.ts
@@ -7,12 +7,6 @@ const API_URL = process.env.BASE_URL
 
 
 
-function getCookie(name: string): string | null {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
-  return null;
-}
 
 
 
